e43ac86ef960ff4d7a2e9589c1aefa86f46a14d8
Umar <khanumar0108@gmail.com>
2025-03-01 17:06:47 +0530

    add letter count functionality and type hints to stats functions

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from stats import get_book_text, get_word_count
+from stats import get_book_text, get_word_count,get_letter_count
 
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
     word_count = get_word_count(book_text)
     print(f"Word count: {word_count}")
+    letter_count = get_letter_count(book_text)
+    print(f"Letter count: {letter_count}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,4 @@
-def get_book_text(filepath):
+def get_book_text(filepath:str) -> str:
     """
     Reads the content of a book from the given file path and returns it.
 
@@ -9,7 +9,7 @@ def get_book_text(filepath):
         book_text = file.read()
         return book_text
 
-def get_word_count(book_text : str):
+def get_word_count(book_text : str) -> int:
     """
     Returns the number of words in the given book text.
 
@@ -18,3 +18,15 @@ def get_word_count(book_text : str):
     """
     words = book_text.split()
     return len(words)
+
+def get_letter_count(book_text : str):
+    book_text_lowercase = book_text.lower()
+    letter_count = {}
+    for letter in book_text_lowercase:
+        if letter.isalpha():
+            if letter in letter_count:
+                letter_count[letter] += 1
+            else:
+                letter_count[letter] = 1
+
+    return letter_count
